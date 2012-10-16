898f1ce80514a4abebedac5374c75a1192764aa7
Josh Pasqualetto <josh.pasqualetto@sonian.net>
2012-10-15 18:47:33 -0700

    Working aggregate display

--- app/models/aggregate.rb
+++ app/models/aggregate.rb
@@ -1,2 +1,12 @@
 class Aggregate < Resting
+  def self.full_hash
+    resp_hash = {}
+    Aggregate.all.each do |agg|
+      resp_hash[agg.check] = {}
+      agg.issued.each do |issue|
+        resp_hash[agg.check][issue] = Aggregate.get("aggregates/#{agg.check}/#{issue}", true)
+      end
+    end
+    resp_hash
+  end
 end
