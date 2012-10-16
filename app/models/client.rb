5018d6a203b753b03b786d7760e1c884c326d734
Josh Pasqualetto <josh.pasqualetto@sonian.net>
2012-10-15 17:25:37 -0700

    Active resource makes me cry, kill it quick

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,39 +1,8 @@
-class Client < ActiveResource::Base
-  include ActiveResource::Extend::WithoutExtension
-  self.site = APP_CONFIG['api']
-
-  #
-  # You cant use a .get here or GET /client/<id> because then you have
-  # to set .collection_name to 'client' and it will blow up subsequent
-  # queries when Client.all is called
-  #
-  def self.single(query)
-    Client.all.select{|client| client.name == query}[0]
-  end
+class Client < Resting
 
   def self.full_hash
     clienthash = {}
     Client.all.each{|c| clienthash[c.name] = { :address => c.address, :environment => c.environment}}
     clienthash
-  end
-
-  def subscriptions
-    self.attributes['subscriptions']
-  end
-
-  def name
-    self.attributes['name']
-  end
-
-  def primary_role
-    self.attributes['primary_role']
-  end
-
-  def timestamp
-    self.attributes['timestamp']
-  end
-
-  def environment
-    self.attributes['environment']
   end
 end
