b9dd8c8b1e93b2c8d0803db23d57563f262237b0
Alan Sebastian <asebastian2@gmail.com>
2013-04-18 11:29:09 -0700

    Add feature specs for stashes

---
+++ spec/features/stashes_features_spec.rb
@@ -0,0 +1,33 @@
+require 'spec_helper'
+
+describe "Stashes" do
+  before :all do
+    load "#{Rails.root}/db/seeds.rb"
+  end
+
+  before :each do
+    user = FactoryGirl.create(:user)
+    user.add_role :admin
+    sign_in_user(user)
+    visit '/stashes'
+  end
+
+  it "should show the stashes page" do
+    page.should have_content "Stashes"
+  end
+
+  it "should show the correct stash count" do
+    page.should have_content "Stashes (#{Stash.all.count})"
+  end
+
+  it "should show stashes and correct data" do
+    page.should have_content "i-424242"
+    page.should have_content "Never"
+    page.should have_content time_ago_in_words(Time.at(1364332102))
+  end
+
+  it "should have a delete link" do
+    page.should have_button "Delete"
+  end
+
+end
