ab801ddffb92c3b46a7bbcd7b30c8efacb31a84a
Alan Sebastian <asebastian2@gmail.com>
2013-05-01 10:05:19 -0700

    Add unit tests for client

--- spec/unit/client_spec.rb
+++ spec/unit/client_spec.rb
@@ -2,11 +2,16 @@ require 'spec_helper'
 
 describe Client do
 
-  # TODO: this entire class needs a test double for Resting 
-  # which can receive messages
-  it "has a valid mock" do
-    client = double('Client')
+  it "should return all clients through cache" do
+    clients = Client.all_with_cache
+    clients.count.should eq 1
   end
-  pending "This test requires a Resting mock"
+
+  it "should return all clients as a hash" do
+    clients_hash = Client.full_hash
+    clients_hash.should be_a Hash
+    clients_hash.should_not be_empty
+    clients_hash.count.should eq 1
+  end
 
 end
