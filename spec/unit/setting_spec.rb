07e6a8065df3a73b63e6243405e7edc4ae7cec9a
Alan Sebastian <asebastian2@gmail.com>
2013-05-01 16:20:45 -0700

    Add unit tests or setting

--- spec/unit/setting_spec.rb
+++ spec/unit/setting_spec.rb
@@ -1,7 +1,21 @@
 require 'spec_helper'
 
-class SettingTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+describe Setting do
+
+  before :each do
+    load "#{Rails.root}/db/seeds.rb"
+  end
+
+  it "should return the api server" do
+    api_server = Setting.api_server
+    api_server.should_not be_nil
+    api_server.should_not be_false
+    Setting.api_server.should_not be_nil
+  end
+
+  it "should return true if environments are in use" do
+    env_setting = Setting.find_by_name("use_environments")
+    Setting.use_environments?.should be_false
+  end
+
 end
