af154dd8eca964bbe6d6e93b651b8e009ace3f31
Alan Sebastian <asebastian2@gmail.com>
2013-05-01 16:39:03 -0700

    Add api spec

--- spec/unit/api_spec.rb
+++ spec/unit/api_spec.rb
@@ -2,5 +2,32 @@ require 'spec_helper'
 
 describe Api do
 
-  pending "this spec requries a Resting mock"
+  before :each do
+    load "#{Rails.root}/db/seeds.rb"
+  end
+
+  it "should return a status" do
+    status = Api.status
+    status.should be_a Api
+    status.should_not be_nil
+    status.should_not be_false
+  end
+
+  it "should return a version" do
+    version = Api.version
+    version.should_not be_false
+    version.should_not be_nil
+  end
+
+  it "should return redis health" do
+    redis_health = Api.redis_health
+    redis_health.should_not be_false
+    redis_health.should_not be_nil
+  end
+
+  it "should return rabbit health" do
+    rabbit_health = Api.rabbitmq_health
+    rabbit_health.should_not be_nil
+  end
+
 end
