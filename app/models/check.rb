ff176cf3816f62b0c97ef3e98f8deb2a19c053d0
Hiroaki Sano <hiroaki.sano.9stories@gmail.com>
2014-05-12 18:25:23 +0900

    fix issue #111 (adapt check.rb to latest sensu api version 0.12)

--- app/models/check.rb
+++ app/models/check.rb
@@ -1,8 +1,8 @@
 class Check < Resting
 
   def self.submit_check(check, subscribers)
-    poster = post("check/request", {:check => check, :subscribers => subscribers })
-    poster.code == 201
+    poster = post("request", {:check => check, :subscribers => subscribers })
+    poster.code == 202
   end
 
   def method_missing(method, *args, &block)
